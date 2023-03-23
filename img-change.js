ee6b59fcbc831e2d02728b1ca52db7904da8f1e5
Dioney Junior <dioneyfbjunior@gmail.com>
2023-03-23 17:58:41 -0300

    Trocar imagens quando selecionar o input do dado

---
+++ img-change.js
@@ -0,0 +1,106 @@
+function d4Img() {
+  var image = document.querySelector("#d4Img");
+  image.src = "/img/d4.png";
+}
+
+function d6Img() {
+  var image = document.querySelector("#d6Img");
+  image.src = "/img/d6.png";
+}
+
+function d8Img() {
+  var image = document.querySelector("#d8Img");
+  image.src = "/img/d8.png";
+}
+function d10Img() {
+  var image = document.querySelector("#d10Img");
+  image.src = "/img/d10.png";
+}
+
+function d12Img() {
+  var image = document.querySelector("#d12Img");
+  image.src = "/img/d12.png";
+}
+
+function d20Img() {
+  var image = document.querySelector("#d20Img");
+  image.src = "/img/d20.png";
+}
+
+function d100Img() {
+  var image = document.querySelector("#d100Img");
+  image.src = "/img/d100.png";
+}
+
+function changed4() {
+  var image = document.querySelector("#d4Img");
+  image.src = "/img/d4_check.png";
+  d6Img();
+  d8Img();
+  d10Img();
+  d12Img();
+  d20Img();
+  d100Img();
+}
+
+function changed6() {
+  var image = document.querySelector("#d6Img");
+  image.src = "/img/d6_check.png";
+  d4Img();
+  d8Img();
+  d10Img();
+  d12Img();
+  d20Img();
+  d100Img();
+}
+
+function changed8() {
+  var image = document.querySelector("#d8Img");
+  image.src = "/img/d8_check.png";
+  d4Img();
+  d6Img();
+  d10Img();
+  d12Img();
+  d20Img();
+  d100Img();
+}
+function changed10() {
+  var image = document.querySelector("#d10Img");
+  image.src = "/img/d10_check.png";
+  d4Img();
+  d6Img();
+  d8Img();
+  d12Img();
+  d20Img();
+  d100Img();
+}
+function changed12() {
+  var image = document.querySelector("#d12Img");
+  image.src = "/img/d12_check.png";
+  d4Img();
+  d6Img();
+  d8Img();
+  d10Img();
+  d20Img();
+  d100Img();
+}
+function changed20() {
+  var image = document.querySelector("#d20Img");
+  image.src = "/img/d20_check.png";
+  d4Img();
+  d6Img();
+  d8Img();
+  d10Img();
+  d12Img();
+  d100Img();
+}
+function changed100() {
+  var image = document.querySelector("#d100Img");
+  image.src = "/img/d100_check.png";
+  d4Img();
+  d6Img();
+  d8Img();
+  d10Img();
+  d12Img();
+  d20Img();
+}
