c3e7a9a0ba6419c8e58ba18c7d4cb60156b4497c
Dioney Junior <dioneyfbjunior@gmail.com>
2023-03-24 06:37:21 -0300

    Melhoria na função de trocar a imagem do input

--- img-change.js
+++ img-change.js
@@ -1,106 +1,54 @@
-function d4Img() {
-  var image = document.querySelector("#d4Img");
-  image.src = "/img/d4.png";
-}
-
-function d6Img() {
-  var image = document.querySelector("#d6Img");
-  image.src = "/img/d6.png";
-}
-
-function d8Img() {
-  var image = document.querySelector("#d8Img");
-  image.src = "/img/d8.png";
-}
-function d10Img() {
-  var image = document.querySelector("#d10Img");
-  image.src = "/img/d10.png";
-}
-
-function d12Img() {
-  var image = document.querySelector("#d12Img");
-  image.src = "/img/d12.png";
-}
-
-function d20Img() {
-  var image = document.querySelector("#d20Img");
-  image.src = "/img/d20.png";
-}
-
-function d100Img() {
-  var image = document.querySelector("#d100Img");
-  image.src = "/img/d100.png";
+function imgReset() {
+  var imaged4 = document.querySelector("#d4Img");
+  imaged4.src = "/img/d4.png";
+  var imaged6 = document.querySelector("#d6Img");
+  imaged6.src = "/img/d6.png";
+  var imaged8 = document.querySelector("#d8Img");
+  imaged8.src = "/img/d8.png";
+  var imaged10 = document.querySelector("#d10Img");
+  imaged10.src = "/img/d10.png";
+  var imaged12 = document.querySelector("#d12Img");
+  imaged12.src = "/img/d12.png";
+  var imaged20 = document.querySelector("#d20Img");
+  imaged20.src = "/img/d20.png";
+  var imaged100 = document.querySelector("#d100Img");
+  imaged100.src = "/img/d100.png";
 }
 
 function changed4() {
-  var image = document.querySelector("#d4Img");
-  image.src = "/img/d4_check.png";
-  d6Img();
-  d8Img();
-  d10Img();
-  d12Img();
-  d20Img();
-  d100Img();
+  imgReset();
+  var imaged4 = document.querySelector("#d4Img");
+  imaged4.src = "/img/d4_check.png";
 }
 
 function changed6() {
-  var image = document.querySelector("#d6Img");
-  image.src = "/img/d6_check.png";
-  d4Img();
-  d8Img();
-  d10Img();
-  d12Img();
-  d20Img();
-  d100Img();
+  imgReset();
+  var imaged6 = document.querySelector("#d6Img");
+  imaged6.src = "/img/d6_check.png";
 }
 
 function changed8() {
-  var image = document.querySelector("#d8Img");
-  image.src = "/img/d8_check.png";
-  d4Img();
-  d6Img();
-  d10Img();
-  d12Img();
-  d20Img();
-  d100Img();
+  imgReset();
+  var imaged8 = document.querySelector("#d8Img");
+  imaged8.src = "/img/d8_check.png";
 }
 function changed10() {
-  var image = document.querySelector("#d10Img");
-  image.src = "/img/d10_check.png";
-  d4Img();
-  d6Img();
-  d8Img();
-  d12Img();
-  d20Img();
-  d100Img();
+  imgReset();
+  var imaged10 = document.querySelector("#d10Img");
+  imaged10.src = "/img/d10_check.png";
 }
 function changed12() {
-  var image = document.querySelector("#d12Img");
-  image.src = "/img/d12_check.png";
-  d4Img();
-  d6Img();
-  d8Img();
-  d10Img();
-  d20Img();
-  d100Img();
+  imgReset();
+  var imaged12 = document.querySelector("#d12Img");
+  imaged12.src = "/img/d12_check.png";
 }
 function changed20() {
-  var image = document.querySelector("#d20Img");
-  image.src = "/img/d20_check.png";
-  d4Img();
-  d6Img();
-  d8Img();
-  d10Img();
-  d12Img();
-  d100Img();
+  imgReset();
+  var imaged20 = document.querySelector("#d20Img");
+  imaged20.src = "/img/d20_check.png";
 }
 function changed100() {
-  var image = document.querySelector("#d100Img");
-  image.src = "/img/d100_check.png";
-  d4Img();
-  d6Img();
-  d8Img();
-  d10Img();
-  d12Img();
-  d20Img();
+  imgReset();
+  var imaged100 = document.querySelector("#d100Img");
+  imaged100.src = "/img/d100_check.png";
 }
